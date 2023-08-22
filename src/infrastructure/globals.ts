8be94c432e54cd30bd202175d8e5e49a6e0c5857
Fragly <hendrik.hps.strydom@gmail.com>
2023-08-22 17:26:57 +0200

    added time

--- src/infrastructure/globals.ts
+++ src/infrastructure/globals.ts
@@ -10,4 +10,10 @@ declare global {
 
 global.webserviceURL = "https://mylms.vossie.net/webservice/rest/server.php"
 
+global.time = {
+    minute: 60 * 1000,
+    hour: 60 * 60 * 1000,
+    day: 24 * 60 * 60 * 1000,
+}
+
 export { }
